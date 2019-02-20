b8814add59259601d43d1e4484a1f4b36c960cc7
Robert Edwards <robertedwardsmail@gmail.com>
2019-02-20 21:56:38 +0200

    Add particl support

---
+++ BTCPayServer/BTCPayNetworkProvider.Particl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BTCPayServer.Services.Rates;
+using NBitcoin;
+using NBXplorer;
+
+namespace BTCPayServer
+{
+    public partial class BTCPayNetworkProvider
+    {
+        public void InitParticl()
+        {
+            var nbxplorerNetwork = NBXplorerNetworkProvider.GetFromCryptoCode("PART");
+            Add(new BTCPayNetwork()
+            {
+                CryptoCode = nbxplorerNetwork.CryptoCode,
+                DisplayName = "Particl",
+                BlockExplorerLink = NetworkType == NetworkType.Mainnet ? "https://explorer.particl.io/tx/{0}" : "https://explorer-testnet.particl.io/tx/{0}",
+                NBitcoinNetwork = nbxplorerNetwork.NBitcoinNetwork,
+                NBXplorerNetwork = nbxplorerNetwork,
+                UriScheme = "particl",
+                DefaultRateRules = new[] 
+                {
+                                "PART_X = PART_BTC * BTC_X",
+                                "PART_BTC = bittrex(PART_BTC)"
+                },
+                CryptoImagePath = "imlegacy/particl.svg",
+                DefaultSettings = BTCPayDefaultSettings.GetDefaultSettings(NetworkType),
+                CoinType = NetworkType == NetworkType.Mainnet ? new KeyPath("44'") : new KeyPath("1'")
+            });
+        }
+    }
+}
